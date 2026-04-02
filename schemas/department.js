0f6003a3cda035a07e98a8922d549082e20310e3
MHie-it <leminhhiep.co@gmail.com>
2026-04-02 20:14:10 +0700

    creact full schema

---
+++ schemas/department.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const departmentSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      uppercase: true
+    },
+
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      default: ""
+    },
+
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "employees",
+      default: null
+    },
+
+    isDeleted: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("departments", departmentSchema);
---
+++ schemas/leave_request.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+
+const leaveRequestSchema = new mongoose.Schema(
+    {
+        employee: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "employees",
+            required: true
+        },
+
+        leaveType: {
+            type: String,
+            enum: ["ANNUAL", "SICK", "UNPAID", "OTHER"],
+            required: true
+        },
+
+        fromDate: {
+            type: Date,
+            required: true
+        },
+
+        toDate: {
+            type: Date,
+            required: true
+        },
+
+        totalDays: {
+            type: Number,
+            min: 0,
+            default: 0
+        },
+
+        reason: {
+            type: String,
+            default: ""
+        },
+
+        status: {
+            type: String,
+            enum: [
+                "PENDING",
+                "REJECTED_MANAGER",
+                "APPROVED_MANAGER",
+                "PENDING_HR",
+                "REJECTED_HR",
+                "APPROVED"
+            ],
+            default: "PENDING"
+        },
+
+        managerActionBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            default: null
+        },
+
+        managerActionAt: {
+            type: Date,
+            default: null
+        },
+
+        managerNote: {
+            type: String,
+            default: ""
+        },
+
+        hrActionBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            default: null
+        },
+
+        hrActionAt: {
+            type: Date,
+            default: null
+        },
+
+        hrNote: {
+            type: String,
+            default: ""
+        }
+    },
+    { timestamps: true }
+);
+
+
+module.exports = mongoose.model("leave_requests", leaveRequestSchema);
