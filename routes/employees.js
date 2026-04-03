0402aa73e9c912919a646ea155578550a946c07d
ledo <lethanhtiendo345@gmail.com>
2026-04-03 17:15:58 +0700

    Remove comments from routes

--- routes/employees.js
+++ routes/employees.js
@@ -3,22 +3,14 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController');
 const { CheckLogin, CheckRole } = require('../utils/authHandler');
 
-// Danh sách các vai trò coi như thuộc bộ phận HR/Quản trị để vào được CRUD
-const hrRoles = ['HR', 'admin'];
+const Roles = ['HR', 'admin'];
 
-// ==========================================
-// DÀNH CHO EMPLOYEE
-// ==========================================
-// Ai đăng nhập cũng được xem Profile của chính mình
 router.get('/me', CheckLogin, employeeController.getMyProfile);
 
-// ==========================================
-// DÀNH CHO HR / ADMIN
-// ==========================================
-router.get('/', CheckLogin, CheckRole(hrRoles), employeeController.getAllEmployees);
-router.get('/:id', CheckLogin, CheckRole(hrRoles), employeeController.getEmployeeById);
-router.post('/create', CheckLogin, CheckRole(hrRoles), employeeController.createEmployee);
-router.put('/:id', CheckLogin, CheckRole(hrRoles), employeeController.updateEmployee);
-router.delete('/:id', CheckLogin, CheckRole(hrRoles), employeeController.deleteEmployee);
+router.get('/', CheckLogin, CheckRole(Roles), employeeController.getAllEmployees);
+router.get('/:id', CheckLogin, CheckRole(Roles), employeeController.getEmployeeById);
+router.post('/create', CheckLogin, CheckRole(Roles), employeeController.createEmployee);
+router.put('/:id', CheckLogin, CheckRole(Roles), employeeController.updateEmployee);
+router.delete('/:id', CheckLogin, CheckRole(Roles), employeeController.deleteEmployee);
 
 module.exports = router;
