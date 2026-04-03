5a784b0a7d0aba101f1a35c5e8362bade6087295
MHie-it <leminhhiep.co@gmail.com>
2026-04-03 17:03:57 +0700

    position

--- routes/employees.js
+++ routes/employees.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController');
 const { CheckLogin, CheckRole } = require('../utils/authHandler');
 
-// Danh sách các vai trò coi như thuộc bộ phận HR/Quản trị để vào được CRUD
+
 const hrRoles = ['HR', 'admin'];
 
-// ==========================================
-// DÀNH CHO EMPLOYEE
-// ==========================================
-// Ai đăng nhập cũng được xem Profile của chính mình
+
+
 router.get('/me', CheckLogin, employeeController.getMyProfile);
 
-// ==========================================
-// DÀNH CHO HR / ADMIN
-// ==========================================
 router.get('/', CheckLogin, CheckRole(hrRoles), employeeController.getAllEmployees);
 router.get('/:id', CheckLogin, CheckRole(hrRoles), employeeController.getEmployeeById);
 router.post('/create', CheckLogin, CheckRole(hrRoles), employeeController.createEmployee);
